31671477db560296d661e3ea911568c995f57585
Yuji Miranda Kunitake <andrekuni123@gmail.com>
2025-07-12 01:04:59 -0300

    feat: incluir modelo da tabela review.

---
+++ app/models/review.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(
+            Integer,
+            primary_key=True,
+            index=True
+        )
+    customer_name = Column(
+            String(255),
+            nullable=False,
+            index=True
+        )
+    review_text = Column(
+            Date,
+            nullable=False,
+            index=True
+        )
+    evaluation_date = Column(
+        Date,
+        nullable=False,
+        index=True
+    )
+    created_at = Column(
+            DateTime(timezone=True),
+            nullable=False,
+            server_default=func.now(),
+            index=True
+        )
+    updated_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    sentiment = Column(
+        # Enum,
+        nullable=False,
+        index=True
+    )
+
+    def __repr__(self) -> str:
+        """
+        Representação em string do objeto de Review
+
+        Returns:
+            str: Representação legível do objeto contendo ID, cliente,
+                data da avaliação e sentimento.
+        """
+        return (
+            f"<Review(id={self.id}, customer='{self.customer_name}', "
+            f"sentiment='{self.sentiment.value}', date='{self.evaluation_date}'"  # noqa: E501
+        )
